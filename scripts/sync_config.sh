a1427376a966d4941443e1ec020679ff8a60ed0d
Yin shengquan <1113959081@qq.com>
2023-10-21 11:37:28 +0800

    add screensaver plugin in neovim

--- scripts/sync_config.sh
+++ scripts/sync_config.sh
@@ -3,8 +3,8 @@
 rsync -r --exclude=".git" ~/.config/nvim/lua/user ~/dotfile/
 rsync -r ~/scripts ~/dotfile/
 rsync -r ~/.config/kitty ~/dotfile/
+rsync -r ~/.config/ranger ~/dotfile/
 rsync ~/.zshrc ~/dotfile
-rsync ~/.config/ranger/rc.conf ~/dotfile
 
 cd ~/dotfile
 git add .
---
+++ user/plugins/cellular-automaton.lua
@@ -0,0 +1,7 @@
+return {
+  "eandrju/cellular-automaton.nvim",
+  cmd = "CellularAutomaton",
+  keys = {
+    { "<leader><cr>", "<cmd>CellularAutomaton make_it_rain<cr>", desc = "make it rain" },
+  },
+}
